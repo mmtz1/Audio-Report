9eccacff94262661e6d157fa3db5f24691d2d12d
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-05-16 22:39:48 -0700

    Created new folder structure

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
  
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/views'));
+app.use(express.static(__dirname + '/public/controlers'));
 
 app.listen(3000);
 
