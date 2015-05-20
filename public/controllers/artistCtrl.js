bb1a4065e6ce5529a14674eb8385b7c288153a4b
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-05-20 11:03:11 -0700

    Added stars

--- public/controllers/artistCtrl.js
+++ public/controllers/artistCtrl.js
@@ -3,5 +3,18 @@ angular.module('LiveAPP.artist',[])
   $scope.myRating = 
   {number:3}
 }])
-
+.directive("rateYo", function() {
+    return {
+        restrict: "A",
+        scope: {
+            rating: "="
+        },
+        template: "<div id='rateYo'></div>",
+        link: function( scope, ele, attrs ) {
+            console.log(scope.rating.number)
+            $(ele).rateYo({
+                rating: scope.rating.number
+            });
+        }
+    };
 });
