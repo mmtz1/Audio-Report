3ff494edad0bfe04799006a13ad396338099603d
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-06-17 23:56:38 -0700

    Made changes to database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public/lib'));
 app.use(bodyParser())
 
 
-var connection = mysql.createConnection({
+var db_config = mysql.createConnection({
   host:"us-cdbr-iron-east-02.cleardb.net",
   user:"b00955d08fef04",
   password:"9bd21f2d",
@@ -29,6 +29,49 @@ var connection = mysql.createConnection({
 //     database    : 'live',
 //     multipleStatements: true
 // });
+
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 connection.connect(function(err){
