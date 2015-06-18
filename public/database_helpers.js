98a0bdac7a16b3d0510366322a9bf658295caa50
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-06-18 00:19:23 -0700

    Removed helperssss

--- public/database_helpers.js
+++ public/database_helpers.js
@@ -11,12 +11,47 @@ var javascripthelp = require('./js/functionHelpers.js')
 //     multipleStatements: true
 // });
 
-var connection = mysql.createConnection({
+var db_config = {
   host:"us-cdbr-iron-east-02.cleardb.net",
   user:"b00955d08fef04",
   password:"9bd21f2d",
   database:"heroku_fdeff37a1f83aa6"
-});
+}
+
+// var db_config = mysql.createConnection({
+//     host        : 'localhost',
+//     port        :  3306,
+//     user        : 'root',
+//     password    : '',
+//     database    : 'live',
+//     multipleStatements: true
+// });
+
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 exports.checkDbArtist = function(req,res,next){
   
