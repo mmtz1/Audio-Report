ca9e9ba2c9f401a855b291984b6372ef71cae11b
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-07-02 09:19:06 -0700

    Fixed database

--- public/database_helpers.js
+++ public/database_helpers.js
@@ -2,21 +2,21 @@ var mysql = require('mysql');
 var express = require('express');
 var javascripthelp = require('./js/functionHelpers.js');
 var dateformat = require('dateformat');
-var connection = mysql.createConnection({
-    host        : 'localhost',
-    port        :  3306,
-    user        : 'root',
-    password    : '',
-    database    : 'live',
-    multipleStatements: true
-});
-
 // var connection = mysql.createConnection({
-//   host:"us-cdbr-iron-east-02.cleardb.net",
-//   user:"b00955d08fef04",
-//   password:"9bd21f2d",
-//   database:"heroku_fdeff37a1f83aa6"
+//     host        : 'localhost',
+//     port        :  3306,
+//     user        : 'root',
+//     password    : '',
+//     database    : 'live',
+//     multipleStatements: true
 // });
+
+var connection = mysql.createConnection({
+  host:"us-cdbr-iron-east-02.cleardb.net",
+  user:"b00955d08fef04",
+  password:"9bd21f2d",
+  database:"heroku_fdeff37a1f83aa6"
+});
 
 exports.checkDbArtist = function(req, res, next){
   if(req.query.getArtist){
